4b8273f0b800674033489ebc1f0b7b7deb7b0c3e
PBudala <PBudala@its.jnj.com>
2024-03-20 15:42:54 +0530

    AddedImage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
-import './HomePage.css'
+import './HomePage.css';
+import homesvg from './Assets/homesvg.svg'; // Ensure the correct path to homesvg.svg
+
 const HomePage = () => {
     const navigate = useNavigate();
 
@@ -12,6 +14,8 @@ const HomePage = () => {
     return (
         <div className="container">
             <h1 className="heading">Foodie Match</h1>
+            {/* Use the imported image */}
+            <img src={homesvg} alt="Foodie Match" style={{ height: "300px" }} />
             <p className="subheading">Find Your Perfect Foodie Partner :)</p>
             <button className="get-started-btn" onClick={handleClick}>Get Started</button>
         </div>
