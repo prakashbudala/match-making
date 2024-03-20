7f826c569210c8c97f35c324e3373851ac5fdb57
PBudala <PBudala@its.jnj.com>
2024-03-20 15:06:03 +0530

    fixed warnings

--- src/YourMatch.js
+++ src/YourMatch.js
@@ -75,7 +75,7 @@ function YourMatch() {
                 }
             });
         };
-    }, []);
+    }, [arraysToCompare]);
 
 
     // Initialize chartRefs array and render charts when matchPercentages change
