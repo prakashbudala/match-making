2332d08755df3c217aae4cf82e519718dd19adf3
PBudala <PBudala@its.jnj.com>
2024-03-27 18:26:19 +0530

    added avatar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,12 +5,12 @@ import Avatar from './Assets/avatar.png'; // Assuming this is the correct path t
 
 function Navbar() {
     const [showPopup, setShowPopup] = useState(false); // State to manage popup visibility
-    const user = sessionStorage.getItem("formData") ? JSON.parse(sessionStorage.getItem("formData")).name : "User";
     const handleLogout = () => {
         sessionStorage.removeItem("loggedIn");
         sessionStorage.removeItem("formData");
         setShowPopup(false);
     };
+
 
     const togglePopup = () => {
         setShowPopup(!showPopup);
@@ -23,14 +23,9 @@ function Navbar() {
             <Link to="/food-preferences" className="nav-link" activeClassName="active">Food Preferences</Link>
             <Link to="/yourmatch" className="nav-link" activeClassName="active">Your Match</Link>
             <div className="avatar-container">
-                <p style={{ color: "#fffff" }} onClick={togglePopup}> Hey {user}</p>
+                <p style={{ color: "#fffff" }} onClick={togglePopup}> </p>
+                {showPopup && sessionStorage.getItem("loggedIn") && <Link to="/" className="nav-link" activeClassName="active" onClick={handleLogout}>Logout</Link>}
                 <img src={Avatar} alt="Avatar" className="nav-link" onClick={togglePopup} />
-                {showPopup && sessionStorage.getItem("formData") && (
-                    < >
-                        <Link to="/" className="nav-link" activeClassName="active" onClick={handleLogout}>Log Out</Link>
-
-                    </>
-                )}
             </div>
         </div>
     );
